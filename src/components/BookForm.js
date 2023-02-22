3275ee078659c26b8fd043d0cac8d36b14fbad5a
simrahnb <65752120+simrahnb@users.noreply.github.com>
2023-02-22 11:20:09 +0500

    Added a button in BookForm to add the book

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,26 +1,27 @@
 import { useState } from "react";
 
 export default function BookForm() {
-    const [newBook, setNewBook] = useState('')
+  const [newBook, setNewBook] = useState("");
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        console.log(newBook)
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(newBook);
 
-        setNewBook('')
-    }
+    setNewBook("");
+  };
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <label>
-                <span>Add a new book title:</span>
-                <input 
-                    required
-                    type="text"
-                    onChange={(e) => setNewBook(e.target.value)}
-                    value={newBook}
-                />
-            </label>
-        </form>
-    )
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        <span>Add a new book title:</span>
+        <input
+          required
+          type="text"
+          onChange={(e) => setNewBook(e.target.value)}
+          value={newBook}
+        />
+      </label>
+      <button>Add</button>
+    </form>
+  );
 }
